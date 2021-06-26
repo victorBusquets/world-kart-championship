b2cf5473a69c6665302e8e1d4afea8d60742f56e
Victor <victor.busquets.boro@gmail.com>
2021-06-26 18:21:36 +0200

    feat: apply team component

--- src/app/modules/player-detail/player-detail.module.ts
+++ src/app/modules/player-detail/player-detail.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlayerDetailComponent } from './components/player-detail/player-detail.component';
 import { PlayerDetailRoutingModule } from './player-detail-routing.module';
+import { TeamLogoModule } from '../shared';
 
 @NgModule({
 	declarations: [PlayerDetailComponent],
-	imports: [CommonModule, PlayerDetailRoutingModule]
+	imports: [CommonModule, PlayerDetailRoutingModule, TeamLogoModule]
 })
 export class PlayerDetailModule {}
